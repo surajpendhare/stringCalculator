b72b867d9afdebad4ba45d3e6c005c1b469d5013
surajpendhare <surajpendhare71@gmail.com>
2024-11-22 13:28:48 +0530

    multiple numbers sum

--- App.js
+++ App.js
@@ -11,6 +11,11 @@ function add(numbers) {
 // console.assert(add("2") === 1, 'Test failed: add("2") should return 2');
 // console.log(add("1"))
 
-console.assert(add("1,2") === 3, 'Test failed: add("1,2") should return 3');
-console.assert(add("1,2") === 2, 'Test failed: add("1,2") should return 3');
-console.log(add("1, 2"));
+// console.assert(add("1,2") === 3, 'Test failed: add("1,2") should return 3');
+// console.assert(add("1,2") === 2, 'Test failed: add("1,2") should return 3');
+// console.log(add("1, 2"));
+
+console.assert(add("1,2,3") === 6, 'Test failed: add("1,2,3") should return 6');
+console.assert(add("5,10,15,20, 25, 30") === 110, 'Test failed: add("10,20,30,40") should return 105');
+console.log(add("1,2,3"));
+console.log(add("5,10,15,20, 25, 30"));
