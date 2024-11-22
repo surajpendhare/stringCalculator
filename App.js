76191671d0335626ad4d48f71d8b2fe5294b3747
surajpendhare <surajpendhare71@gmail.com>
2024-11-22 13:14:37 +0530

    success and failed test case

--- App.js
+++ App.js
@@ -5,3 +5,7 @@ function add(numbers) {
 
 console.assert(add("") === 0, 'Test failed: add("") should return 0');
 console.log(add("0"))
+
+console.assert(add("1") === 1, 'Test failed: add("1") should return 1');
+console.assert(add("2") === 1, 'Test failed: add("2") should return 2');
+console.log(add("1"))
