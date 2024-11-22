7fe1f210a075aa008d085ecab7b197929f78cef0
surajpendhare <surajpendhare71@gmail.com>
2024-11-22 13:31:42 +0530

    new line delimiters

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 function add(numbers) {
   if (!numbers) return 0;
-  const numsArr = numbers.split(",").map((item) => parseInt(item));
+  const numsArr = numbers.split(/,|\n/).map((item) => parseInt(item));
   return numsArr.reduce((sum, num) => sum + num, 0);
 }
 
@@ -15,7 +15,11 @@ function add(numbers) {
 // console.assert(add("1,2") === 2, 'Test failed: add("1,2") should return 3');
 // console.log(add("1, 2"));
 
-console.assert(add("1,2,3") === 6, 'Test failed: add("1,2,3") should return 6');
-console.assert(add("5,10,15,20, 25, 30") === 110, 'Test failed: add("10,20,30,40") should return 105');
-console.log(add("1,2,3"));
-console.log(add("5,10,15,20, 25, 30"));
+// console.assert(add("1,2,3") === 6, 'Test failed: add("1,2,3") should return 6');
+// console.assert(add("5,10,15,20, 25, 30") === 110, 'Test failed: add("10,20,30,40") should return 105');
+// console.log(add("1,2,3"));
+// console.log(add("5,10,15,20, 25, 30"));
+
+console.assert(add("1\n2,3") === 6, 'Test failed: add("1\n2,3") should return 6');
+console.assert(add("4\n5\n6") === 15, 'Test failed: add("4\n5\n6") should return 15');
+console.log(add("1\n2,3"));
