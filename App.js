aaf96d5c3e5a931133fbc44b86dd0a752eda70ee
surajpendhare <surajpendhare71@gmail.com>
2024-11-22 13:25:48 +0530

    two number sum failed success testcase

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 function add(numbers) {
-    if (!numbers) return 0;
-    const numsArr = numbers.split(',').map((item)=>parseInt(item));
-    return numsArr.reduce((sum, num) => sum + num, 0);
-
-   
-    // const  numbers = numsArr.map(Number)
+  if (!numbers) return 0;
+  const numsArr = numbers.split(",").map((item) => parseInt(item));
+  return numsArr.reduce((sum, num) => sum + num, 0);
 }
 
 // console.assert(add("") === 0, 'Test failed: add("") should return 0');
@@ -14,5 +11,6 @@ function add(numbers) {
 // console.assert(add("2") === 1, 'Test failed: add("2") should return 2');
 // console.log(add("1"))
 
-console.log(add("1, 2"))
-
+console.assert(add("1,2") === 3, 'Test failed: add("1,2") should return 3');
+console.assert(add("1,2") === 2, 'Test failed: add("1,2") should return 3');
+console.log(add("1, 2"));
